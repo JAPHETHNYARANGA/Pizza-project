4176629a8d6e14058843d2fe0bcba24e3fb9daa0
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-08-01 01:21:30 +0300

    finalized the essential parts of the project

--- java.js
+++ java.js
@@ -5,26 +5,65 @@ open.addEventListener('click', ()=>{
     sideBar.style.width="250px";
 })
 
-var price , crustprice, toppingprice;
-let total = 0;
-
-function getOrder(name, size, crust, topping, total){
-    this.name = name;
-    this.size= size;
-    this.crust = crust;
-    this.topping=topping;
-    this.total=total;
-}
 
 
-$(document).ready(function(){
-    $("button.checkout").click(function(event){
-        var pizzaname =$(".pizzaname option:selected").val();
-        var pizzasize =$(".pizzasize option:selected").val();
-        var pizzacrust=$(".pizzacrust option:selected").val();
-        var pizzatopping = [];
-        $.each($("input[name='toppings']:checked"), function(){            
-            pizzatopping.push($(this).val());
-
-    })
-})
+$(document).ready(function() {
+      $("#text-center").submit(function(event) {
+        
+        function pizzaname() {
+          var pizzaFlavour = document.getElementById("pizzaname").value;
+          return parseInt(pizzaFlavour);
+        }
+       
+        function orderNumber() {
+            var orderNumber = document.getElementById("orderNumber").value;
+            return parseInt(orderNumber);
+          }
+        function pizzacrust() {
+          var pizzacrust = document.getElementById("pizzacrust").value;
+          return parseInt(pizzacrust);
+        }
+        function pizzasize() {
+            var pizzasize = document.getElementById("pizzasize").value;
+            return parseInt(pizzasize);
+          }
+        function pizzatopping() {
+          var pizzatopping = document.getElementById("pizzatopping").value;
+          return parseInt(pizzatopping);
+        }
+       
+    
+        
+        function Order(pizzaname, pizzasize, pizzacrust, pizzatopping, orderNumber) {
+          this.newpizzaname = pizzaname;
+          this.newpizzasize = pizzasize;
+          this.newpizzacrust = pizzacrust;
+          this.newpizzatopping = pizzatopping;
+          this.neworderNumber = orderNumber;
+        }
+    
+     
+        var getInput = new Order(pizzaname(), pizzasize(), pizzacrust(), pizzatopping(), orderNumber());
+    
+        var totalCost =
+          (getInput .newpizzasize +
+            getInput .newpizzacrust +
+            getInput .newpizzatopping +
+            getInput .newpizzaname) *
+            getInput .neworderNumber;
+    
+        var finalCost = parseInt(totalCost + 200);
+        alert("Your charges for Pizza" + totalCost);
+        if (confirm('Do you want delivery?')) {
+            var name = prompt("Please Enter Your location")
+            alert('Your pizza will be delivered to ' + name + ' your total cost is ksh ' + finalCost);
+          } else {
+            // Do nothing!
+            alert("Please pick your pizza from Nyaranga's Hub");
+          }
+       
+        $("#myform").reset();
+    
+        event.preventDefault();
+      });
+    });
