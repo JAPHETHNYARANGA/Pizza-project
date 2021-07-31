ee7fa4d562f6843f3c272ab61076760c22428082
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-07-31 21:39:31 +0300

    started js section

--- java.js
+++ java.js
@@ -4,3 +4,27 @@ let sideBar = document.querySelector('.navigation');
 open.addEventListener('click', ()=>{
     sideBar.style.width="250px";
 })
+
+var price , crustprice, toppingprice;
+let total = 0;
+
+function getOrder(name, size, crust, topping, total){
+    this.name = name;
+    this.size= size;
+    this.crust = crust;
+    this.topping=topping;
+    this.total=total;
+}
+
+
+$(document).ready(function(){
+    $("button.checkout").click(function(event){
+        var pizzaname =$(".pizzaname option:selected").val();
+        var pizzasize =$(".pizzasize option:selected").val();
+        var pizzacrust=$(".pizzacrust option:selected").val();
+        var pizzatopping = [];
+        $.each($("input[name='toppings']:checked"), function(){            
+            pizzatopping.push($(this).val());
+
+    })
+})
